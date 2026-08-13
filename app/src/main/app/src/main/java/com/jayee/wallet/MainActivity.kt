72011d74f761f268e6123490a5e9870d834e4665
insightmoneymanagement015-ctrl <insightmoneymanagement015@gmail.com>
2026-08-14 05:36:30 +0800

    Update MainActivity.kt

--- app/src/main/app/src/main/java/com/jayee/wallet/MainActivity.kt
+++ app/src/main/app/src/main/java/com/jayee/wallet/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.jayee.wallet
 
+import android.app.Activity
 import android.os.Bundle
+import android.graphics.Color
+import android.view.Gravity
 import android.widget.TextView
-import androidx.activity.ComponentActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val textView = TextView(this)
+
         textView.text = "JAYEE Wallet"
         textView.textSize = 28f
+        textView.setTextColor(Color.BLACK)
+        textView.gravity = Gravity.CENTER
         textView.setPadding(40, 40, 40, 40)
 
         setContentView(textView)
